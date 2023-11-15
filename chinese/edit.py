88372729452046b3d0afc4c8d41c0f0bd967843e
Gustaf Carefall <106698658+Gustaf-C@users.noreply.github.com>
2023-11-15 12:11:52 +0800

    Fix failing tests

--- chinese/edit.py
+++ chinese/edit.py
@@ -17,8 +17,8 @@
 # You should have received a copy of the GNU General Public License along with
 # Chinese Support 3.  If not, see <https://www.gnu.org/licenses/>.
 
-import anki.notes
-import aqt.editor
+import anki
+import aqt
 from anki.hooks import addHook
 from aqt import gui_hooks, mw
 
